f5225e79eaa1967f5f816eb3b20b01a901e09cbd
Majid <mkazimi2@my.bcit.ca>
2024-10-29 17:04:36 -0700

    Create firebase database.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 function getNameFromAuth() {
     firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
         if (user) {
             // Do something for the currently logged-in user here: 
             console.log(user.uid); //print the uid in the browser console
